acc418f1e5902dadb4472d8ef3ef8c32569129a4
Bryan Lynch <bml312@nyu.edu>
2019-06-25 11:42:09 +0900

    commit all

--- board.rb
+++ board.rb
@@ -5,7 +5,7 @@ require 'byebug'
 
 class Board
   # @@file = 'sudoku1.txt'
-  @@file = 'test_win.txt'
+  @@file = 'sudoku2.txt'
   @@empty_grid = Array.new(9){Array.new(9)}
 
 
@@ -43,30 +43,57 @@ class Board
   end
   #
   def won?
-    win = [1,2,3,4,5,6,7,8,9]
+    sorted_nums = [1,2,3,4,5,6,7,8,9]
     val_grid = self.dup
-    row_align?(val_grid, win) && col_align?(val_grid,win)
+    row_align?(val_grid, sorted_nums) && col_align?(val_grid,sorted_nums) && square_align?(val_grid,sorted_nums)
   end
 
-  def row_align?(grid, win)
-    grid.each{|row| return false unless row.sort == win}
+  def row_align?(grid, sorted_nums)
+    grid.each{|row| return false unless row.sort == sorted_nums}
     true
   end
 
 
-  def col_align?(grid, win)
+  def col_align?(grid, sorted_nums)
     (grid.length).times do |i|
       col = []
       grid.each do |row|
         col << row[i]
       end
-      return false unless col.sort == win
+      return false unless col.sort == sorted_nums
     end
     true
   end
 
-  # def square_align?(win)
-  # end
+  def square_align?(grid, sorted_nums)
+    start_row = 0
+    start_col = 0
+    while start_row < 9
+      while start_col < 9
+        hold = []
+        square = get_square(start_row, start_col)
+        square.each do |pos|
+          row, col = pos
+          hold << grid[row][col]
+        end
+        return false unless hold.sort == sorted_nums
+        start_col += 3
+      end
+        start_row += 3
+    end
+    true
+  end
+
+  def get_square(row, col)
+    pivot_row = row
+    pivot_col = col
+    square = [
+              [pivot_row, pivot_col],      [pivot_row, pivot_col + 1],     [pivot_row, pivot_col + 2],
+              [pivot_row + 1, pivot_col],  [pivot_row + 1, pivot_col + 1], [pivot_row + 1, pivot_col + 2],
+              [pivot_row + 2, pivot_col],  [pivot_row + 2, pivot_col + 1], [pivot_row + 2, pivot_col + 2]
+            ]
+    square
+  end
 
 
   def dup
---
+++ lulu_ex.py
@@ -0,0 +1,9 @@
+print 'Hello world I am lulu'
+
+x = 3
+y = 10
+
+if x == 3
+	print 'lulu so happy'
+else
+	print 'not so happy'
